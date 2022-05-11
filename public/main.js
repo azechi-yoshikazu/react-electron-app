05affffc66a8d7e2849100f6b7d33689983735e9
YoshikazuAzechi <azechi.yoshikazu@guildstudio.co.jp>
2022-05-11 14:51:28 +0900

    Run react app on electron window.

---
+++ public/main.js
@@ -0,0 +1,38 @@
+﻿const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const url = require('url')
+
+let mainWindow
+
+function createWindow() {
+  mainWindow = new BrowserWindow({ width: 800, height: 600 })
+
+  const launchUrl = process.env.ELECTRON_LAUNCH_URL || url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  })
+  mainWindow.loadURL(launchUrl)
+
+  if (process.env.ELECTRON_LAUNCH_URL) {
+    mainWindow.webContents.openDevTools()
+  }
+
+  mainWindow.on('closed', _ => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', _ => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', _ => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
